bcc5f3ef15b03d8769ade25e0861f01befee53eb
Rodrigo Thenopholo <thenopholo92@gmail.com>
2025-11-16 11:10:18 -0300

    feat: Add Chi router dependency and implement server initialization in main API

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"github.com/thenopholo/go-bid/internal/config"
+	"github.com/thenopholo/go-bid/internal/server"
+)
+
 func main() {
-  
+	logger := config.NewLogger("MAIN_API")
+	logger.Info("Starting application...")
+	InitServer()
+}
+
+func InitServer() {
+	server.NewServer().Start()
 }
