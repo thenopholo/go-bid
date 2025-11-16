681b666a2513d779532550e72e4cdae576042c72
Rodrigo Thenopholo <thenopholo92@gmail.com>
2025-11-16 20:22:13 -0300

    feat: Add UserSignup handler to user management functionality

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -3,5 +3,6 @@ package handler
 import "net/http"
 
 func (h *Handler) UserSignup(w http.ResponseWriter, r *http.Request) {}
+
 func (h *Handler) UserLogin(w http.ResponseWriter, r *http.Request) {}
 func (h *Handler) UserLogout(w http.ResponseWriter, r *http.Request) {}
